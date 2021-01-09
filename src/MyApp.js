b18653abb8934f6d8ccdb0bdee4df01682ccf454
eflore53 <59632517+eflore53@users.noreply.github.com>
2021-01-08 16:15:22 -0800

    added deleted button

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Table from './Table'
+import React, {useState} from 'react';
+import Table from './Table';
 
 const characters = [
     {
@@ -21,11 +21,25 @@ const characters = [
 ];
 
 function MyApp() {
-    return (
-        <div className="container">
-          <Table characterData={characters} />
-        </div>
-      );  
+   const [characters, setCharacters] = useState([  
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+        // the rest of the data
+      },
+    ]);
+
+   function removeOneCharacter (index) {
+      const updated = characters.filter((character, i) => {
+         return i !== index
+      });
+      setCharacters(updated);
+   }
+   return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    </div>
+  ) 
 }
 
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -15,9 +15,12 @@ function TableBody (props) {
     const rows = props.characterData.map((row, index) => {
     return (
         <tr key={index}>
-            <td>{row.name}</td>
-            <td>{row.job}</td>
-        </tr>
+  <td>{row.name}</td>
+  <td>{row.job}</td>
+  <td>
+    <button onClick={() => props.removeCharacter(index)}>Delete</button>
+  </td>
+</tr>
         );
     });
     return (
@@ -28,12 +31,12 @@ function TableBody (props) {
 }
   
 function Table(props) {
-    return(
-        <table>
-            <TableHeader/>
-            <TableBody characterData={props.characterData} />
-        </table>
+    return (
+      <table>
+        <TableHeader />
+        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
+      </table>
     );
- }
+  }
 
 export default Table;
