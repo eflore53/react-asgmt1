ecc5d44cf1b7b8df05fc65bc3f5fb9f2a111c1ee
eflore53 <59632517+eflore53@users.noreply.github.com>
2021-01-15 14:49:08 -0800

    axios added

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,38 +1,53 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Table from './Table';
 import Form from './Form';
-
-const characters = [
-    {
-      name: 'Charlie',
-      job: 'Janitor',
-    },
-    {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
-      name: 'Dee',
-      job: 'Aspring actress',
-    },
-    {
-      name: 'Dennis',
-      job: 'Bartender',
-    },
-];
+import axios from 'axios';
 
 function MyApp() {
-    const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
-   function removeOneCharacter (index) {
-      const updated = characters.filter((character, i) => {
-         return i !== index
-      });
-      setCharacters(updated);
-   }
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index
+    });
+    setCharacters(updated);
+  }
 
-   function updateList(person) {
-    setCharacters([...characters, person]);
+   function updateList(person) { 
+    makePostCall(person).then( result => {
+    if (result)
+      setCharacters([...characters, person] );
+    });
+  }
+
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+        setCharacters(result);
+     });
+  }, [] );
+
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;     
+    }
+    catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+    }
+ }
+
+  async function makePostCall(person){
+    try {
+      const response = await axios.post('http://localhost:5000/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
   }
   
    return (
