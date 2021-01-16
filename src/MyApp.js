97fe922940cea5028a4ac95a4e6f3d8a9d994b04
eflore53 <59632517+eflore53@users.noreply.github.com>
2021-01-15 18:37:31 -0800

    delete linked to backend added

--- src/MyApp.js
+++ src/MyApp.js
@@ -10,13 +10,17 @@ function MyApp() {
     const updated = characters.filter((character, i) => {
       return i !== index
     });
-    setCharacters(updated);
+    makeDeleteCall (index).then( result => {
+    if (result.status === 204)
+      setCharacters(updated)
+    })
+    ;
   }
 
    function updateList(person) { 
     makePostCall(person).then( result => {
-    if (result)
-      setCharacters([...characters, person] );
+    if (result.status === 201)
+      setCharacters([...characters, result.data] );
     });
   }
 
@@ -42,6 +46,17 @@ function MyApp() {
   async function makePostCall(person){
     try {
       const response = await axios.post('http://localhost:5000/users', person);
+      return response;
+    }
+    catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  async function makeDeleteCall(index){
+    try {
+      const response = await axios.delete('http://localhost:5000/users/'+ characters[index].id);
       return response;
     }
     catch (error) {
